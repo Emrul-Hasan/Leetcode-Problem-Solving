1e3d232c1f8ee74becd7e5e02206716f8c347a17
Emrul Hasan Emon <72728517+Emrul-Hasan@users.noreply.github.com>
2023-08-19 23:32:40 +0600

    Time: 53 ms (72.25%), Space: 42.1 MB (48.22%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    for (let i = m, j = 0; j < n; i++, j++) {
+       nums1[i] = nums2[j];
+   }
+   nums1.sort((a, b) => a - b);
+   return[nums1];
+};
+nums1 = [1,2,3,0,0,0];
+m = 3;
+nums2 = [2,5,6];
+n = 3;
+console.log(merge(nums1,m,nums2,n));
