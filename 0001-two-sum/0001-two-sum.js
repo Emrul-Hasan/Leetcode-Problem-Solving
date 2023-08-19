19274668aac6e7b33ebd5b808763df91d147abb1
Emrul Hasan Emon <72728517+Emrul-Hasan@users.noreply.github.com>
2023-08-19 23:13:09 +0600

    Time: 73 ms (56.89%), Space: 43.2 MB (25.40%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = new Map();
+    for(let i=0; i<nums.length; i++){
+        const complement = target - nums[i];
+        if(map.has(complement))
+            return [map.get(complement), i];
+        map.set(nums[i], i);
+    }
+    return [];
+};
+
+console.log(twoSum([2,7,11,15], [9]))
