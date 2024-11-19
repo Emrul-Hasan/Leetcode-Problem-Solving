1601766bebe2ed2339255ccdd17651f469e1c8d1
Emrul Hasan Emon <72728517+Emrul-Hasan@users.noreply.github.com>
2024-11-19 15:16:57 +0600

    Time: 6 ms (26.01%), Space: 51.5 MB (33.3%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,22 @@
+var isValid = function(s) {
+    let stack = [];
+    for (let c of s){
+        if (c === '(' || c === '{' || c === '[' ){
+            stack.push(c);
+        }
+        else {
+            if(!stack.length || 
+            (c === ')' && stack[stack.length - 1] !== '(') ||
+              (c === '}' && stack[stack.length - 1] !== '{') ||
+                (c === ']' && stack[stack.length - 1] !== '[') ){
+                    return false;
+                }
+                stack.pop();
+        }
+    }
+     console.log(stack);
+
+    return !stack.length
+
+};
+   
